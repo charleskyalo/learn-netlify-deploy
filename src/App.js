ec3040bb9270f399c467247bd81528dcff83b90a
charles kyalo <charleskyalo689@gmail.com>
2020-12-24 18:14:51 +0300

    my experience

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Deploying A site using React and Netlify</h2>
+        <h3>its easy peasy! for sure</h3>
       </header>
     </div>
   );
